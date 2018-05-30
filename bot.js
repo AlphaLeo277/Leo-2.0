6ccd8fea7c4001102d05475a2a9c57898de84ad4
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-05-30 20:57:58 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const newUsers = []; 
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -41,6 +43,23 @@ client.on('message', message => {
     	message.reply('( ͡° ͜ʖ ͡°)');
   	}
      
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
 });
 
 // THIS  MUST  BE  THIS  WAY
