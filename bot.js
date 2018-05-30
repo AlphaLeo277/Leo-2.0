c4b5f113b27e6584a7ce01593fd05b9762ab10ce
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-05-30 12:11:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,6 +29,17 @@ client.on('message', message => {
     if (message.content === 'Boosie') {
     	message.reply('No its Leo 2.0');
   	}
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        
+        args = args.splice(1);
+        switch(cmd) {
+                case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
 });
 
 // THIS  MUST  BE  THIS  WAY
