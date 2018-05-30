ae3d66d46d77686082a80c06743ba9fc9a312e21
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-05-30 13:02:02 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,9 @@ client.on('message', message => {
     if (message.content === 'Oops') {
     	message.reply('XD you made a mistake');
   	}
+    if (message.content === 'Thank you Leo 2.0') {
+    	message.reply('You are welcome ^v^');
+  	}
     
 });
 
