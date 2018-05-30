881ac5272d5f696da574f029b912b22347942116
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-05-30 18:12:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ client.on('message', message => {
     	message.reply('You are welcome ^v^');
   	}
      if (message.content === 'Hide and seek rules') {
-    	message.reply('Hide and Seek Rules');
+    	message.reply('Hide and Seek Rules', 'no squidbagging');
     }
 });
 
