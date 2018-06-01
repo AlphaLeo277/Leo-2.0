7c712157db249faca734a56247eecb8c5ca0e854
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-06-01 21:58:58 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,29 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    def random_game_status():
-	statuses = ["with i7-2670QM", "with mainframes", "with Cleverbot",
-	"tic-tac-toe with Joshua", "tic-tac-toe with WOPR", "the Turing test",
-	"with my memory", "with R2-D2", "with C-3PO", "with BB-8",
-	"with machine learning", "gigs", "with Siri", "with TARS", "with KIPP",
-	"with humans", "with Skynet", "Goldbach's conjecture",
-	"Goldbach's conjecture solution", "with quantum foam",
-	"with quantum entanglement", "with P vs NP", "the Reimann hypothesis",
-	"the Reimann proof", "with the infinity gauntlet", "for the other team",
-	"hard to get", "to win", "world domination", "with Opportunity",
-	"with Spirit in the sand pit", "with Curiousity", "with Voyager 1",
-	"music", "Google Ultron", "not enough space here to",
-	"the meaning of life is", "with the NSA", "with neural networks", 
-	"with RSS Bot", "with Data", "with Harmon", " "]
-	me = discord.utils.find(lambda s: s != None, client.servers).me
-	if not me:
-		return
-	elif not me.game:
-		updated_game = discord.Game(name = random.choice(statuses))
-	else:
-		updated_game = me.game
-		updated_game.name = random.choice(statuses)
-	await client.change_presence(game = updated_game) 
+    client.user.setActivity("Splatoon 2: Octo Expansion");
 });
 
 client.on('message', message => {  
