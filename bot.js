622eca8057106c6b3f6332eacdfa582725e32d3c
AlphaLeo277 <39750797+AlphaLeo277@users.noreply.github.com>
2018-06-01 21:40:12 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-     client.user.setGame('GAME HERE')
+     client.user.setPresence({ game: { name: 'with discord.js' }, status: 'online' })
 });
 
 client.on('message', message => {  
